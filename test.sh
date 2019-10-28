57d56c04d1b67a87338e0d9df7e920e99fc0da66
蜗壳 <yantao@innotechx.com>
2019-10-28 17:43:08 +0800

    Hello

--- test.sh
+++ test.sh
@@ -14,24 +14,24 @@ then
     tag=$1
 fi
 
-cfile="coverage.txt"
+tfile="coverage.txt"
 hfile="coverage.html"
-echo "" > $cfile
+echo "" > $tfile
 echo "" > $hfile
 
 if [ "${tag}" != "leetcli" -a $(go list ./src/... | grep $tag | wc -l) == 1 ];
 then
     # 如果符合条件的只有一个测试用例, 测试之, 生成覆盖率可视 html
     f=$(go list ./src/... | grep $tag)
-    go test -v -coverprofile=$cfile -covermode=atomic $f
-    go tool cover -html=$cfile -o $hfile
+    go test -v -coverprofile=$tfile -covermode=atomic $f
+    go tool cover -html=$tfile -o $hfile
 else
     # 执行全部测试用例
     for d in $(go list ./src/... | grep -v vendor); do
         go test -coverprofile=$hfile -covermode=atomic $d
         if [ -f $hfile ];
         then
-            cat $hfile >> $cfile
+            cat $hfile >> $tfile
         fi
     done
 fi
